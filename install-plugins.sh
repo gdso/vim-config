20ca363810b0fafc53c12e0a0f1075ccec2cd8bb
Gregory de Souza <gdesouza@gmail.com>
2013-05-18 13:22:53 -0700

    adding coffeescript plugin

--- install-plugins.sh
+++ install-plugins.sh
@@ -9,21 +9,24 @@ cd ~/.vim/bundle/
 echo deleting contents of ~/.vim/bundle/
 rm -rf *
 
-echo cloning solarized  colorscheme
+echo 
 git clone git://github.com/altercation/vim-colors-solarized.git
 
-echo cloning nerdtree
+echo 
 git clone git://github.com/scrooloose/nerdtree.git
 
-echo cloning vimerl
+echo 
 git clone git://github.com/jimenezrick/vimerl.git
 
-echo cloning vim-less
+echo 
+git clone git://github.com/kchmck/vim-coffee-script.git 
+
+echo
 git clone git://github.com/groenewege/vim-less.git
 
-echo cloning tagbar
+echo 
 git clone git://github.com/majutsushi/tagbar.git
 
 
-echo cloning ack.vim
+echo 
 git clone git://github.com/mileszs/ack.vim.git
